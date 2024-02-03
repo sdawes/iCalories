b1e3c9274fd1a9b9352641154e05c55b0ca1383a
Stephen Dawes <stephenrdawes@gmail.com>
2024-02-03 11:32:57 +0000

    Initial Commit

---
+++ iCalories/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iCalories
+//
+//  Created by Stephen Dawes on 03/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iCalories/iCaloriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCaloriesApp.swift
+//  iCalories
+//
+//  Created by Stephen Dawes on 03/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct iCaloriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
