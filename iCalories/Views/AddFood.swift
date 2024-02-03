1d493361e0a362d8deb570ea036142fdeb1a7337
Stephen Dawes <stephenrdawes@gmail.com>
2024-02-03 12:16:16 +0000

    Second commit for initial set up for data controllers and views

--- iCalories/Views/AddFood.swift
+++ iCalories/Views/AddFood.swift
@@ -8,8 +8,32 @@
 import SwiftUI
 
 struct AddFood: View {
+    @Environment (\.managedObjectContext) var managedObjContext
+    @Environment(\.dismiss) var dismiss
+    
+    @State private var name = ""
+    @State private var calories: Double = 0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            Section {
+                TextField("Food name", text: $name)
+                
+                VStack {
+                    Text("Calories: \(Int(calories))")
+                    Slider(value: $calories, in: 0...1000, step: 10)
+                }
+                .padding()
+                
+                HStack {
+                    Spacer()
+                    Button("Submit") {
+                        DataController().addFood(name: name, calories: calories, context: managedObjContext)
+                        dismiss()
+                    }
+                }
+            }
+        }
     }
 }
 
